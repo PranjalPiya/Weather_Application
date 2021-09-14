27aaa16ea9a6a35658be3e945a7809549f2025ab
Pranjalpiya <pranjalpiya10@gmail.com>
2021-09-14 22:31:06 +0545

    second update

--- lib/fetch_weather.dart
+++ lib/fetch_weather.dart
@@ -9,7 +9,7 @@ class Fetch {
   static Future<Weather>? weatherData({String? cityWeather = "city"}) async {
     http.Response response = await http.get(
       Uri.parse(
-          'http://api.weatherapi.com/v1/current.json?key=8b89699e4fe8416b81464324213108&q=$cityWeather&aqi=no'),
+          '//api.weatherapi.com/v1/current.json?key=8b89699e4fe8416b81464324213108&q=$cityWeather&aqi=no'),
     );
     var results = jsonDecode(response.body);
     var data = Weather.fromJson(results);
